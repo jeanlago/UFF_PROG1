48fbd98eb8421f925d1f0ba777b10d592ca3b425
Jean Lago <100036946+jeanlago@users.noreply.github.com>
2023-09-22 16:54:26 -0300

    TESTE

--- Bhaskara_Beecrowd_1036.py
+++ Bhaskara_Beecrowd_1036.py
@@ -1,2 +1,11 @@
 
-VAL1, VAL2, VAL3 = map(int(input()))
+lista = []
+for i in range(3):
+    lista[i] = int(input(""))
+
+DELTA = lista[2] - 4*lista[1]*lista[3]
+
+X1 = (-lista[2] +DELTA) /(2*lista[1])
+X2 = (-lista[2] -DELTA) / (2*lista[1])
+
+print(f"{X1} {X2}")
